efb44115310dcedf2685c46890a7999414a383e9
unknown <gwayden@yandex.ru>
2025-08-20 22:28:11 +0500

    employer said TDD so i wrote tests first hahahah

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Contracts;
+using System.IO;
+
+public class Test
+{
+    public string Expression { get; }
+    public string Answer { get; }
+    public Test(string expr, string ans)
+    {
+        this.Expression = expr;
+        this.Answer = ans;
+    }
+    public override string ToString()
+    {
+        return $"{this.Expression}={this.Answer}";
+    }
+}
+public class CalcTesting
+{
+    public static List<Test> ParseTests(string path)
+    {
+        string[] text = File.ReadAllLines(path);
+        List<Test> parsed_tests = new();
+        foreach (string line in text)
+        {
+            /// криво написано, но для начала пойдёт
+            string expr = line.Split('=')[0];
+            string ans = line.Split('=')[1];
+            parsed_tests.Add(new Test(expr, ans));
+        }
+        return parsed_tests;
+    }
+    public static void Main(string[] args)
+    {
+        string path = @"C:\Users\galah\Desktop\byndy-assignment\test.txt";
+        List<Test> tests = ParseTests(path);
+        Console.WriteLine(tests[0]);
+    }
+}
--- obj/Debug/net9.0/byndy-assignment.AssemblyInfo.cs
+++ obj/Debug/net9.0/byndy-assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("byndy-assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de67fa51033d30279e21723e6027c5f54f37f03b")]
 [assembly: System.Reflection.AssemblyProductAttribute("byndy-assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("byndy-assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
